8fea4323089e7c02c1de0a67135dc69d04afe9d3
wudanyang <wudanyang@baidu.com>
2025-02-03 01:07:43 +0800

    up

--- src/helpers/userSetup.js
+++ src/helpers/userSetup.js
@@ -7,6 +7,14 @@ function userEleventySetup(eleventyConfig) {
   // Feel free to add any plugin you want here instead of /.eleventy.js
   const debug = require("eleventy-plugin-debug");
   eleventyConfig.addPlugin(debug);
+
+  eleventyConfig.addCollection("sortedPosts", collectionApi => {
+    return collectionApi.getFilteredByGlob("src/site/notes/**/*.md").sort((a, b) => {
+      // 按日期倒序排序（最新的排在最前面）
+      return new Date(b.data.created) - new Date(a.data.created);
+    });
+  });
+
 }
 exports.userMarkdownSetup = userMarkdownSetup;
 exports.userEleventySetup = userEleventySetup;
