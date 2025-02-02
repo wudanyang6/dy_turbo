5074fe4d94e5325e22e85c5123c14c6c87e89bd1
wudanyang <wudanyang@baidu.com>
2025-02-03 00:00:03 +0800

    up

--- src/helpers/userSetup.js
+++ src/helpers/userSetup.js
@@ -5,6 +5,8 @@ function userMarkdownSetup(md) {
 function userEleventySetup(eleventyConfig) {
   // The eleventyConfig parameter stands for the the config instantiated in /.eleventy.js.
   // Feel free to add any plugin you want here instead of /.eleventy.js
+  const debug = require("eleventy-plugin-debug");
+  eleventyConfig.addPlugin(debug);
 }
 exports.userMarkdownSetup = userMarkdownSetup;
 exports.userEleventySetup = userEleventySetup;
